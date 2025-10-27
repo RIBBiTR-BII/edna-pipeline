ca85e2c16dc6e07ae3ecf198dfc2e596c1e1b44d
cob-staines <cob.staines@gmail.com>
2025-10-27 18:41:48 -0500

    fix file path bug

--- 16S Process TEST/yyyy-mm-dd_sequencing/source/r/00_make_manifest_file.R
+++ 16S Process TEST/yyyy-mm-dd_sequencing/source/r/00_make_manifest_file.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # create manifest file in tabular form
-tibble(`absolute-filepath` = c(list.files('sequences'), recursive = TRUE)) %>%
+tibble(`absolute-filepath` = c(list.files('sequences', recursive = TRUE))) %>%
   mutate(`sample-id` = str_match(`absolute-filepath`, "(Pe-[0-9A-Z-]*)_S[0-9]*_L[0-9]*_R[12]")[,2],
          `absolute-filepath` = paste0("$PWD/sequences/", `absolute-filepath`),
          `direction` = if_else(str_detect(`absolute-filepath`, "_R1_"), "forward", 'reverse')) %>%
