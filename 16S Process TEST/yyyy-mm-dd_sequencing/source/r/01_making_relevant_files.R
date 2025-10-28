6796cc83e630f93d046fdef1e326981b27e77ee0
cob-staines <cob.staines@gmail.com>
2025-10-27 19:13:53 -0500

    revert to original workflow

--- 16S Process TEST/yyyy-mm-dd_sequencing/source/r/01_making_relevant_files.R
+++ 16S Process TEST/yyyy-mm-dd_sequencing/source/r/01_making_relevant_files.R
@@ -9,25 +9,26 @@ library(phylotools)
 library(readxl)
 library(openxlsx)
 
+
 ## Read in the .biom feature table. 
 feature_table <-
-  read_tsv(here(wddir, "analysis/denoised_16S_eDNA/sample_table/feature-table.tsv"), skip = 1) %>%
-    as.data.frame() %>%
-    rename("ASV_ID" = "#OTU ID")
+  read_tsv(paste0("analysis/denoised_16S_eDNA/sample_table/feature-table.tsv"), skip = 1) %>%
+  as.data.frame() %>%
+  rename("ASV_ID" = "#OTU ID")
 
 ## Read in taxonomic classifications 
 taxonomy_table <-
-  read.table(paste0(wddir, "/analysis/denoised_16S_eDNA/classified_taxonomy/classified_taxonomy/", 
-          list.files(here(wddir, 'analysis/denoised_16S_eDNA/classified_taxonomy/classified_taxonomy')), 
-          "/data/taxonomy.tsv"),
-          header = T, 
-          sep = '\t')
+  read.table(paste0("analysis/denoised_16S_eDNA/classified_taxonomy/classified_taxonomy/", 
+                    list.files('analysis/denoised_16S_eDNA/classified_taxonomy/classified_taxonomy'), 
+                    "/data/taxonomy.tsv"),
+             header = T, 
+             sep = '\t')
 
 # Read in DNA sequences 
 sequence_table <-
-  read.fasta(file = paste0(wddir, "/analysis/denoised_16S_eDNA/representative_sequences/", 
-         list.files(here(wddir, 'analysis/denoised_16S_eDNA/representative_sequences')), 
-         "/data/dna-sequences.fasta"))%>%
+  read.fasta(file = paste0("analysis/denoised_16S_eDNA/representative_sequences/", 
+                           list.files('analysis/denoised_16S_eDNA/representative_sequences'), 
+                           "/data/dna-sequences.fasta"))%>%
   rename(ASV_ID = seq.name, 
          sequence = seq.text)
 
@@ -36,4 +37,4 @@ list_of_datasets <- list("Feature Table" = feature_table,
                          "Taxonomy Table" = taxonomy_table, 
                          "Sequence Table" = sequence_table)
 
-write.xlsx(list_of_datasets, file = here(wddir, "output/collated_eDNA_data.xlsx"))
+write.xlsx(list_of_datasets, file = "output/collated_eDNA_data.xlsx")
