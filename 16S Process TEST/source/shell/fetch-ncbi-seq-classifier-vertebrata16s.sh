e82100651a473f9f728242b9c4a216fd86d41d7c
cob-staines <cob.staines@gmail.com>
2025-11-05 15:17:26 -0500

    clean and isolate classifier fetch script

--- 16S Process TEST/source/shell/fetch-ncbi-seq-classifier-vertebrata16s.sh
+++ 16S Process TEST/source/shell/fetch-ncbi-seq-classifier-vertebrata16s.sh
@@ -6,56 +6,62 @@ source activate qiime2-amplicon-2025.4
 # make sure to have rescript installed within this environment
 # see details here: https://library.qiime2.org/plugins/rescript/27/
 
-cd analysis
+if [ -d "taxonomic-classification" ]; then
+  echo "The existing "vertebrata16s-classifier" folder will be deleted. Proceed? (y/n)"
+  read confirm
+  if [[ "$confirm" == [Yy] ]]; then
+    rm -r "vertebrata16s-classifier"
+    echo "Folder deleted."
+  else
+    echo "Process canceled."
+    exit 1
+  fi
+fi
 
 # make a new dirctory for the NCBI sequences
-mkdir 16S_classifier
+mkdir vertebrata16s-classifier
+cd vertebrata16s-classifier
 
-# change to NCBI sequences directory
-cd 16S_classifier
+mkdir temp
 
 # Download all vertebrate (txid7742) 16S sequences from NCBI
 echo "downloading vertibrate 16S sequences from NCBI"
 qiime rescript get-ncbi-data \
 --p-query "(txid7742[ORGN] AND (mitochondria[TITLE] OR mitochondrion[TITLE] OR mitochondrial[TITLE])) OR (txid7742[ORGN] AND (large subunit ribosomal RNA[TITLE] OR 16S rRNA[TITLE] OR 16S ribosomal RNA[TITLE] OR 16S[TITLE] OR 16S r RNA[TITLE] OR MT-RNR2[TITLE] OR MTRNR2[TITLE] OR RNR2[TITLE])) AND (biomol_genomic[PROP] AND ddbj_embl_genbank[filter] AND mitochondrion[filter])" \
---output-dir Vertebrata16S
+--output-dir temp
 
 #remove sequences with 5 or more degen sequences and homopolymers longer than 12
 echo "filtering sequences"
 qiime rescript cull-seqs \
-    --i-sequences ./Vertebrata16S/sequences.qza \
+    --i-sequences ./temp/sequences.qza \
     --p-num-degenerates 5 \
     --p-homopolymer-length 12 \
-    --o-clean-sequences Vertebrata16S_ambi_hpoly_filtd_seqs.qza
+    --o-clean-sequences ./temp/Vertebrata16S_ambi_hpoly_filtd_seqs.qza
 
 # remove any replicates
 echo "removing replicates"
 qiime rescript dereplicate --verbose \
-  --i-sequences ./Vertebrata16S/sequences.qza \
-  --i-taxa ./Vertebrata16S/taxonomy.qza \
+  --i-sequences ./temp/sequences.qza \
+  --i-taxa ./temp/taxonomy.qza \
   --p-mode 'super' \
   --p-derep-prefix \
   --p-rank-handles ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species'] \
-  --o-dereplicated-sequences Vertebrata16S_derep1_seqs.qza \
-  --o-dereplicated-taxa Vertebrata16S_derep1_taxa.qza
+  --o-dereplicated-sequences ./temp/Vertebrata16S_derep1_seqs.qza \
+  --o-dereplicated-taxa ./temp/Vertebrata16S_derep1_taxa.qza
 
 # extract sequences with the primer in its entirety and remove any sequences larger or smaller than expected.
 echo "extracting sequences"
 qiime feature-classifier extract-reads \
---i-sequences Vertebrata16S_derep1_seqs.qza \
+--i-sequences ./temp/Vertebrata16S_derep1_seqs.qza \
 --p-f-primer ACGAGAAGACCCYRTGGARCTT \
 --p-r-primer ATCCAACATCGAGGTCGTAA \
 --p-min-length 150 \
 --p-max-length 450 \
 --o-reads Vertebrata16S_derep1_seqs_extracted.qza
 
-# extract reads to check
-qiime tools extract \
---input-path Vertebrata16S_derep1_seqs_extracted.qza \
---output-path Vertebrata16S_derep1_seqs_extracted
-
-qiime tools extract \
---input-path Vertebrata16S_derep1_taxa.qza \
---output-path Vertebrata16S_derep1_taxa 
+qiime rescript filter-taxa \
+    --i-taxonomy ./temp/Vertebrata16S_derep1_taxa.qza \
+    --m-ids-to-keep-file Vertebrata16S_derep1_seqs_extracted.qza \
+    --o-filtered-taxonomy Vertebrata16S_derep1_taxa_extracted.qza
 
 echo "done creating classifier"
