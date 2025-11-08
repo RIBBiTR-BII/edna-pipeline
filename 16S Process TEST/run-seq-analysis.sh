c37472c05053c5c85dfce80ccf816a38cfa0c29a
cob-staines <cob.staines@gmail.com>
2025-11-07 21:42:58 -0500

    move metadata to output, add exit status codes

--- 16S Process TEST/run-seq-analysis.sh
+++ 16S Process TEST/run-seq-analysis.sh
@@ -8,11 +8,11 @@ c_update_taxa=$(yq e '.taxonomy.updateBool' config.yml)
 echo "Initiating run: $c_run_name"
 
 # prompt user to drop folders if preexisting
-if [ -d $c_run_dir/metadata ]; then
+if [ -d $c_run_dir/output ]; then
   echo "The existing metadata, analysis and output folders will be deleted from the run directory ($c_run_dir). Proceed? (y/n)"
   read confirm
   if [[ "$confirm" == [Yy] ]]; then
-    rm -r "$c_run_dir/metadata" "$c_run_dir/analysis" "$c_run_dir/output"
+    rm -r "$c_run_dir/analysis" "$c_run_dir/output"
     echo "Folders deleted."
   else
     echo "Process canceled."
@@ -21,12 +21,12 @@ if [ -d $c_run_dir/metadata ]; then
 fi
 
 echo "Creating run directories"
-mkdir "$c_run_dir/metadata" "$c_run_dir/analysis" "$c_run_dir/output"
+mkdir "$c_run_dir/analysis" "$c_run_dir/output" "$c_run_dir/output/metadata"
 
 # copy config file to metadata
-cp config.yml "$c_run_dir/metadata"
+cp config.yml "$c_run_dir/output/metadata"
 # save and export config path (copy) as environmental variable
-env_config_path="$c_run_dir/metadata/config.yml"
+env_config_path="$c_run_dir/output/metadata/config.yml"
 export env_config_path
 
 # update reference taxonomy
@@ -35,18 +35,23 @@ if [ "$c_update_taxa" = "true" ]; then
   bash update-vertebrata16s-classifier.sh 
 fi
 
+# r01
 echo "Making manifest file"
 Rscript source/r/00_make_manifest_file.R "$env_config_path"
 
+# r02
 echo "Initiating run metadata"
 Rscript source/r/01_init_run_metadata.R "$env_config_path"
 
+# s##
 echo "Performing sequence processing"
 bash source/shell/qiime-seq-process.sh
 
+# t01
 echo "Finalizing run metadata"
 Rscript source/r/02_final_run_metadata.R "$env_config_path"
 
+# t02
 echo "Consolidating outputs"
 Rscript source/r/03_consolidate_outputs.R "$env_config_path"
 
