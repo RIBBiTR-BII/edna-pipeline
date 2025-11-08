db1921e0e13f373e4e8e2877f851c9511fd507a6
cob-staines <cob.staines@gmail.com>
2025-11-07 22:00:08 -0500

    reorder final metadata

--- 16S Process TEST/source/r/03_final_run_metadata.R
+++ 16S Process TEST/source/r/03_final_run_metadata.R
@@ -17,8 +17,9 @@ start_time = as.POSIXct(metadata$post$runtime$startTimestamp, format = "%Y-%m-%d
 metadata$post$runtime$duration = paste0(as.character(round(difftime(end_time, start_time, units = "secs"), digits = 0)), " seconds")
 # update exit status
 metadata$post$exitStatus = dplyr::case_when(
-  dir.exists(paste0(config$run$runDir, "/analysis/s07_classified_taxonomy_vsearch/search_results/")) ~ "complete",
-  file.exists(paste0(config$run$runDir, "/analysis/s07_classified_taxonomy_vsearch/search_results.qza")) ~ "incomplete-s07",
+  file.exists(paste0(config$run$runDir, "/output/", config$run$name, "_eDNA_result_tables.xlsx")) ~ "complete",
+  dir.exists(paste0(config$run$runDir, "/analysis/s07_classified_taxonomy_vsearch/search_results/")) ~ "incomplete-s07-b",
+  file.exists(paste0(config$run$runDir, "/analysis/s07_classified_taxonomy_vsearch/search_results.qza")) ~ "incomplete-s07-a",
   dir.exists(paste0(config$run$runDir, "/analysis/s06_denoised_16S_eDNA/representative_sequences")) ~ "incomplete-s06",
   file.exists(paste0(config$run$runDir, "/analysis/s05_primertrimmed_16S_eDNA_Demux.qzv ")) ~ "incomplete-s05",
   file.exists(paste0(config$run$runDir, "/analysis/s04_leadlag_primertrimmed_16S_eDNA_Demux.qza")) ~ "incomplete-s04",
@@ -27,7 +28,7 @@ metadata$post$exitStatus = dplyr::case_when(
   file.exists(paste0(config$run$runDir, "/analysis/s01_16S_eDNA_Demux.qza")) ~ "incomplete-s01",
   file.exists(paste0(config$run$runDir, "/output/metadata/run_metadata.yml")) ~ "incomplete-r02",
   file.exists(paste0(config$run$runDir, "/output/metadata/manifest.csv")) ~ "incomplete-r01",
-  .default ~ "incomplete-r00"
+  .default = "incomplete-r00"
 )
 # write to file
 write_yaml(metadata, paste0(config$run$runDir, "/output/metadata/run_metadata.yml"))
