e771e631d6c9624ad652ae0906cb51af02e80638
cob-staines <cob.staines@gmail.com>
2025-11-08 12:02:49 -0500

    add original manifest script

--- 16S Process EDIT/Sequence_Analysis_Folder/source/r/00_make_manifest_file.R
+++ 16S Process EDIT/Sequence_Analysis_Folder/source/r/00_make_manifest_file.R
@@ -1,12 +1,10 @@
 library(tidyverse)
-library(here)
 
 # create manifest file in tabular form
-tibble(`absolute-filepath` = c(list.files(here('16S Process TEST', '2025-10-27_sequencing', 'sequences'), recursive = TRUE))) %>%
-  mutate(`sample-id` = str_replace(`absolute-filepath`, "\\_.*", "")) %>%
-  separate(`sample-id`, sep = "-", into = c("seq-id", "sample-id"), extra = 'merge') %>% 
-  mutate(`absolute-filepath` = paste0("$PWD/sequences/", `absolute-filepath`)) %>%
-  mutate(direction = if_else(str_detect(`absolute-filepath`, "_R1_"), "forward", 'reverse')) %>%
+tibble(`absolute-filepath` = c(list.files('sequences'), recursive = TRUE)) %>%
+  mutate(`sample-id` = str_match(`absolute-filepath`, "(Pe-[0-9A-Z-]*)_S[0-9]*_L[0-9]*_R[12]")[,2],
+         `absolute-filepath` = paste0("$PWD/sequences/", `absolute-filepath`),
+         `direction` = if_else(str_detect(`absolute-filepath`, "_R1_"), "forward", 'reverse')) %>%
   select(`sample-id`, `absolute-filepath`, `direction`) %>%
   mutate(`sample-id` = str_replace(`sample-id`, "-", "_")) %>%
 # write as a csv and place in metadata folder
