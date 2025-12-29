fa90db547c449d9eb8c1417c85ea0eeac1bbc96e
cob-staines <cob.staines@gmail.com>
2025-12-29 18:26:55 -0500

    adjust sample id scraping

--- 16S Process TEST/source/r/00_make_manifest_file.R
+++ 16S Process TEST/source/r/00_make_manifest_file.R
@@ -9,10 +9,10 @@ env_config_path = args[1]
 config = read_yaml(env_config_path)
 
 # create manifest file in tabular form
-tibble(`absolute-filepath` = c(list.files(paste0(config$run$runDir, "/sequences"), recursive = TRUE))) %>%
+peace = tibble(`absolute-filepath` = c(list.files(paste0(config$run$runDir, "/sequences"), recursive = TRUE))) %>%
   filter(str_detect(`absolute-filepath`, "\\.fastq(\\.gz)?$")) %>%
   mutate(`seq_id` = str_match(basename(`absolute-filepath`), "([A-Z]{2}[0-9]{3})-[0-9A-Za-z-]+_S[0-9]+_L[0-9]+_R[12]")[,2],
-         `sample-id` = str_match(basename(`absolute-filepath`), "-([A-Za-z]+-[0-9A-Z-]*)_S[0-9]+_L[0-9]+_R[12]")[,2],
+         `sample-id` = str_match(basename(`absolute-filepath`), "-([A-Za-z]+-[0-9A-Za-z-]*)_S[0-9]+_L[0-9]+_R[12]")[,2],
          `absolute-filepath` = paste0("$PWD/sequences/", `absolute-filepath`),
          `direction` = if_else(str_detect(`absolute-filepath`, "_R1_"), "forward", 'reverse'),
          `sample-id` = if_else(is.na(`sample-id`), `seq_id`, `sample-id`),
