9249de2732d62b963234cf4c4b649883e1db677c
cob-staines <cob.staines@gmail.com>
2026-03-05 08:52:33 -0500

    rename classifier build scripts

--- 16S_sequence_processing/build-vertebrata12s-classifier.sh
+++ 16S_sequence_processing/build-vertebrata12s-classifier.sh
@@ -4,7 +4,7 @@ gene="12S"
 src="./source/shell/fetch-ncbi-seq-classifier-vertebrata12s.sh"
 
 echo "updating taxonomic classifier"
-bash source/shell/fetch-ncbi-seq-classifier-vertebrata12s.sh
+# bash source/shell/fetch-ncbi-seq-classifier-vertebrata12s.sh
 
 echo "writing classifier metadata"
 Rscript source/r/aa_build_classifier_metadata.R "$gene" "$src"
