976244d615c6f07b8b5c7721426fb98bdf1ce831
cob-staines <cob.staines@gmail.com>
2026-05-28 16:02:57 -0500

    allow qc runs

--- 16S_sequence_processing/source/r/00_make_manifest_file.R
+++ 16S_sequence_processing/source/r/00_make_manifest_file.R
@@ -11,20 +11,31 @@ library(yaml)
 # inherit env_config_path
 args = commandArgs(trailingOnly = TRUE)
 env_config_path = args[1]
+qc_run = args[2]
 
-# read in config file
-config = read_yaml(env_config_path)
+qc_run_lgl <- !is.na(qc_run) && isTRUE(as.logical(qc_run))
+
+if (qc_run_lgl) {
+  sequence_dir = dirname(env_config_path)
+  out_dir = paste0(env_config_path, "/manifest.csv")
+  filepath_prefix = "$PWD/"
+} else {
+  config = read_yaml(env_config_path)
+  sequence_dir = paste0(config$run$runDir, "/sequences")
+  out_dir = paste0(config$run$runDir, "/output/metadata/manifest.csv")
+  filepath_prefix = "$PWD/sequences/"
+}
 
 # create manifest file in tabular form
-peace = tibble(`absolute-filepath` = c(list.files(paste0(config$run$runDir, "/sequences"), recursive = TRUE))) %>%
+peace = tibble(`absolute-filepath` = c(list.files(sequence_dir, recursive = TRUE))) %>%
   filter(str_detect(`absolute-filepath`, "\\.fastq(\\.gz)?$")) %>%
   mutate(`seq_id` = str_match(basename(`absolute-filepath`), "([A-Z]{2}[0-9]{3})-[0-9A-Za-z-]+_S[0-9]+_L[0-9]+_R[12]")[,2],
          `sample-id` = str_match(basename(`absolute-filepath`), "-([A-Za-z]+-[0-9A-Za-z-]*)_S[0-9]+_L[0-9]+_R[12]")[,2],
-         `absolute-filepath` = paste0("$PWD/sequences/", `absolute-filepath`),
+         `absolute-filepath` = paste0(filepath_prefix, `absolute-filepath`),
          `direction` = if_else(str_detect(`absolute-filepath`, "_R1_"), "forward", 'reverse'),
          `sample-id` = if_else(is.na(`sample-id`), `seq_id`, `sample-id`),
          `sample-id` = str_replace(`sample-id`, "-", "_")) %>%
   select(`sample-id`, `absolute-filepath`, `direction`) %>%
   # write as a csv and place in metadata folder
-  write_csv(., paste0(config$run$runDir, "/output/metadata/manifest.csv"))
+  write_csv(., out_dir)
 
